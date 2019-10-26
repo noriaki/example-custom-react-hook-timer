80ffd04cf5995b400ea4e00397fe437fa0bbd8e9
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-10-27 01:42:07 +0900

    add actions that start, stop, restart, reset

--- src/components/timer.js
+++ src/components/timer.js
@@ -4,23 +4,45 @@ let timerId = null
 
 export default ({ value, delay }) => {
   const [remaining, setRemaining] = useState(value)
-  const [finishTime, setFinishTime] = useState(Date.now() + value)
+  const [finishTime, setFinishTime] = useState(Date.now() + remaining)
+  const [isActive, setActive] = useState(true)
 
   useEffect(() => {
+    start()
+    return () => stop() // <= componentWillUnmount
+  }, [finishTime]) // <= componentDidMount, when `finishTime` is updated
+
+  const start = () => {
     timerId = setInterval(() => {
       const nextRemaining = finishTime - Date.now()
       if (nextRemaining > 0) {
         setRemaining(nextRemaining)
       } else {
         setRemaining(0)
-        if (timerId !== null) {
-          clearInterval(timerId)
-          timerId = null
-        }
+        stop()
       }
     }, delay)
-    return () => clearInterval(timerId) // <= componentWillUnmount
-  }, []) // <= componentDidMount
+    setActive(true)
+  }
 
-  return <p>あと { remaining } msec</p>
+  const stop = () => {
+    setActive(false)
+    if (timerId !== null) {
+      clearInterval(timerId)
+      timerId = null
+    }
+  }
+
+  const restart = () => setFinishTime(Date.now() + remaining)
+
+  const reset = () => setRemaining(value)
+
+  return (
+    <div>
+      <p>あと { remaining } msec</p>
+      { !isActive && remaining > 0 && <button onClick={restart}>開始</button> }
+      { isActive && <button onClick={stop}>一時停止</button> }
+      { !isActive && remaining !== value && <button onClick={reset}>リセット</button> }
+    </div>
+  )
 }
