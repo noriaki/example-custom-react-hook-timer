a8ba224fc5a91fc223c9095f4e62c4f54ede26e0
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-10-31 05:24:30 +0900

    add more timer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 import Timer from '../components/timer'
 
-export default () => <Timer value={5000} delay={500} />
+export default () => [
+  <Timer key="timer1" value={5000} delay={500} />,
+  <Timer key="timer2" value={50000} delay={100} />
+]
